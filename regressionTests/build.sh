748ed928c01748295e238840d22044bf4b4d1401
a3schuur <a3schuur>
2005-03-15 09:50:09 +0000

    Corrected typo

--- regressionTests/build.sh
+++ regressionTests/build.sh
@@ -19,7 +19,7 @@
 # */
 
 
-dirs='FilesAndDirectories BigOutput ProcessIndications LifeCyleIndications'
+dirs='FilesAndDirectories BigOutput ProcessIndications LifeCycleIndications'
 
 sfcbdir=/usr/local/share/sfcb
 if [ -x /usr/bin/curl ] && [ ! -f $sfcbdir/CIM/CIM_Schema.mof ]

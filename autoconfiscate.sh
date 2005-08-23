b6fecf1ad1b575a2af5f63f6c85b60a59c051a2c
ibmpeanuts <ibmpeanuts>
2005-08-23 22:47:26 +0000

    #1222948

--- autoconfiscate.sh
+++ autoconfiscate.sh
@@ -3,6 +3,7 @@ aclocal &&
 autoheader &&
 libtoolize && 
 automake -af &&
+export enable_debug=yes
 autoconf &&
 #if test -d test; then cd test && ./autoconfiscate.sh && cd ..; fi
 if test -d mofc; then cd mofc && ./autoconfiscate.sh; fi

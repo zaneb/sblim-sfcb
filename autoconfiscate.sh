35c1dedbab5f4e6c9339fb4d9293f67af6868157
ibmpeanuts <ibmpeanuts>
2005-08-24 23:29:12 +0000

    *** empty log message ***

--- autoconfiscate.sh
+++ autoconfiscate.sh
@@ -3,7 +3,6 @@ aclocal &&
 autoheader &&
 libtoolize && 
 automake -af &&
-export enable_debug=yes
 autoconf &&
 #if test -d test; then cd test && ./autoconfiscate.sh && cd ..; fi
 if test -d mofc; then cd mofc && ./autoconfiscate.sh; fi

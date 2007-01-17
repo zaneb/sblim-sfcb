df3b89ebebfd61c6feee67cc0f40c3a60e00b7ee
sschuetz <sschuetz>
2007-01-17 12:04:59 +0000

    [ 1636805 ] sfcb: ain does not work for indication subscriptions

--- providerMgr.c
+++ providerMgr.c
@@ -502,7 +502,10 @@ static ProviderInfo *getAssocProvider(char *className, char *nameSpace)
    
    if (strcasecmp(nameSpace,"root/interop")==0 ||
         strcasecmp(nameSpace,"root/pg_interop")==0) {
-      _SFCB_RETURN(interOpProvInfoPtr);  
+      //_SFCB_RETURN(interOpProvInfoPtr);
+      //changed to "forceNoProvInfoPtr" because interop provider is registered via
+      //the providerRegister for the class cim_indicationsubscription now
+      _SFCB_RETURN(forceNoProvInfoPtr);
    }     
 
    if (interopClass(className)) _SFCB_RETURN(forceNoProvInfoPtr);

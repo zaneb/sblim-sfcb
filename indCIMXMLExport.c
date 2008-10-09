5901d2180049fab0f2d56c56b9cf6c115a56b548
mchasal <mchasal>
2008-10-09 19:18:17 +0000

    2028194 Failure in generating indication

--- indCIMXMLExport.c
+++ indCIMXMLExport.c
@@ -150,7 +150,7 @@ static int genRequest(CurlData *cd, char *url, char **msg)
         return 1;
     }    
     
-    if (!supportsSSL(cd) && strncasecmp(url,"https:",6)) {
+    if (!supportsSSL(cd) && (strncasecmp(url,"https:",6) == 0)) {
         *msg=strdup("This curl library does not support https urls.");
         return 2;
     }    

ed12d842bc9e8acda38d3df246bf34326ba4ae09
mchasal <mchasal>
2008-10-09 19:24:07 +0000

    2027392 Bug in Query processing

--- providerDrv.c
+++ providerDrv.c
@@ -1687,7 +1687,7 @@ CMPIValue queryGetValue(QLPropertySource* src, char* name, QLOpd *type)
    CMPIValue v={(long long)0};
    
    if (rc.rc==CMPI_RC_OK) {
-      if (d.type & CMPI_SINT) { 
+      if ((d.type & CMPI_SINT) == CMPI_SINT) { 
          if (d.type==CMPI_sint32) v.sint64=d.value.sint32;
          else if (d.type==CMPI_sint16) v.sint64=d.value.sint16;
          else if (d.type==CMPI_sint8) v.sint64=d.value.sint8;

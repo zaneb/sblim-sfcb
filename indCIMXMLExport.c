0c4df637e382e2e24f720d82f4c2bf899ede7598
smswehla <smswehla>
2008-10-17 20:20:33 +0000

    [ 2175426 ] possible int overflow

--- indCIMXMLExport.c
+++ indCIMXMLExport.c
@@ -130,7 +130,13 @@ static void initializeHeaders(CurlData *cd)
 static size_t writeCb(void *ptr, size_t size, size_t nmemb, void *stream)
 {
     UtilStringBuffer *sb = (UtilStringBuffer*)stream;
-    int length = size * nmemb;
+    unsigned int length = 0;
+    unsigned long long calcLength = (unsigned long)size * nmemb;
+    if(calcLength > UINT_MAX) {
+        mlogf(M_ERROR, M_SHOW, "--- Cannot allocate for %d members of size $d\n", nmemb, size);
+        return 0;
+    }
+    length = calcLength & UINT_MAX;
     char c=((char*)ptr)[length];
     ((char*)ptr)[length]=0;
     sb->ft->appendChars(sb,(char*)ptr);

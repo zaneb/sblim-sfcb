a596a409f2fd182dad9ccc3a4845ca107d906a19
mchasal <mchasal>
2008-11-18 23:39:41 +0000

    [ 2042150 ] Mismatched CMPI error code and error message     Adding testcase

---
+++ test/wbemcli/check_errors.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+$rc=0;
+
+$output=`wbemcli ei http://localhost:5988/root/cimv2:bogusclass 2>&1 1>/dev/null`;
+unless ($output =~ /CIM_ERR_INVALID_CLASS: Invalid class/ ) {
+    print "\tIncorrect error message for: \"Invalid class\".\n"; 
+    $rc=1;
+}
+
+$output=`wbemcli ei http://localhost:5988/root/bogus:CIM_LogicalElement 2>&1 1>/dev/null`;
+unless ($output =~ /CIM_ERR_INVALID_NAMESPACE: Invalid namespace/ ) {
+    print "\tIncorrect error message for: \"Invalid namespace\".\n"; 
+    $rc=1;
+}
+
+
+$output=`wbemcli bogus http://localhost:5988/root/cimv2:CIM_LogicalElement 2>&1 1>/dev/null`;
+unless ($output =~ /Cmd Exception: Invalid operation/ ) {
+    print "\tIncorrect error message for: \"Invalid operation\".\n"; 
+    $rc=1;
+}
+
+exit $rc;
+
+

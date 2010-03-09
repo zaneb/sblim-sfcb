927cfce76fe1ed5a5d9ed5549b2e89407fbfe7d2
smswehla <smswehla>
2010-03-09 19:59:38 +0000

    [ 2966804 ] setObjectPath returns unitialized pointer

--- instance.c
+++ instance.c
@@ -303,6 +303,7 @@ static CMPIStatus __ift_setObjectPath(CMPIInstance * inst,
       ns = "*NoNameSpace*";
       cn = "*NoClassName*";
       tmp1.rc=tmp2.rc=tmp3.rc=CMPI_RC_OK;
+      tmp1.msg=tmp2.msg=tmp3.msg=NULL;
    }
 
    if (tmp1.rc != CMPI_RC_OK || tmp2.rc != CMPI_RC_OK || tmp3.rc != CMPI_RC_OK) {

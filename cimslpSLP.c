f880f616ff3328c208339181f8bf10ab40c7f010
smswehla <smswehla>
2010-05-27 20:39:42 +0000

    [ 2984485 ] Possible access to freed memory in cimslpSLP.c

--- cimslpSLP.c
+++ cimslpSLP.c
@@ -195,6 +195,7 @@ registerCIMService(cimSLPService css, int slpLifeTime, char **urlsyntax,
   SLPError        callbackerr = 0;
   char           *attrstring;
   int             retCode = 0;
+  int             changed = 0;
 
   _SFCB_ENTER(TRACE_SLP, "registerCIMService");
 
@@ -294,6 +295,7 @@ registerCIMService(cimSLPService css, int slpLifeTime, char **urlsyntax,
         _SFCB_TRACE(2, ("--- SLP deregistration error, *urlsyntax = \"%s\"\n", *urlsyntax));
       free(*gAttrstring);
     }
+    changed = 1;
   }
   err = SLPReg(hslp,
                *urlsyntax,
@@ -317,11 +319,11 @@ registerCIMService(cimSLPService css, int slpLifeTime, char **urlsyntax,
     retCode = callbackerr;
   }
   // only save the last state when something changed to not waste mallocs
-  if (strcmp(attrstring, *gAttrstring)) {
-    *gAttrstring = strdup(attrstring);
+  if(changed) {
+    *gAttrstring = attrstring;
+  } else {
+    free(attrstring);
   }
-
-  free(attrstring);
   freeCSS(css);
 
   SLPClose(hslp);

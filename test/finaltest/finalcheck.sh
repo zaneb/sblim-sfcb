d71792eb9d4c1b39315ddf662241b50d790e5011
buccella <buccella>
2010-06-29 23:46:18 +0000

    [ 2920363 ] make test fails if sfcb is running under valgrind

--- test/finaltest/finalcheck.sh
+++ test/finaltest/finalcheck.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 if ! ps -C sfcbd >/dev/null
 then
-    echo "  FAILED sfcbd no longer running"
-    exit 1
-else
-    echo "=================================================="
-    echo "******** ALL TESTS COMPLETED SUCCESSFULLY ********"
-    echo "=================================================="
-    exit 0
+    if ps aux | grep valgrind | grep sfcbd > /dev/null
+    then
+        echo "  assuming sfcbd is running under valgrind"
+    else
+        echo "  FAILED sfcbd no longer running"
+        exit 1
+    fi
 fi
+echo "=================================================="
+echo "******** ALL TESTS COMPLETED SUCCESSFULLY ********"
+echo "=================================================="
+exit 0
--- test/running.sh
+++ test/running.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 if ! ps -C sfcbd >/dev/null
 then
+    if ps aux |grep valgrind |grep sfcbd > /dev/null
+    then
+	echo "  assuming sfcb is running under valgrind"
+        exit 0
+    fi
     echo "  FAILED sfcbd not running"
     exit 1
 fi

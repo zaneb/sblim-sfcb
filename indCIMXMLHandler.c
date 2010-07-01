abfb9eadaea903545c2affad1eaae831c7f9c2c5
buccella <buccella>
2010-07-01 18:59:26 +0000

    [ 2952628 ] Handler is not removed from interop-Hashtable when CI fails

--- indCIMXMLHandler.c
+++ indCIMXMLHandler.c
@@ -276,8 +276,12 @@ CMPIStatus IndCIMXMLHandlerCreateInstance(CMPIInstanceMI * mi,
    op=CMNewObjectPath(_broker,"root/interop","cim_indicationsubscription",&st);
    rv=CBInvokeMethod(_broker,ctx,op,"_addHandler",in,out,&st);
 
-   if (st.rc==CMPI_RC_OK) 
+   if (st.rc==CMPI_RC_OK) {
       st=InternalProviderCreateInstance(NULL,ctx,rslt,cop,ciLocal);
+   }
+   else {
+     rv=CBInvokeMethod(_broker,ctx,op,"_removeHandler",in,out,NULL);
+   }
 
    CMRelease(ciLocal);
    _SFCB_RETURN(st);

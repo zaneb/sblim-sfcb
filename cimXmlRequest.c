b3fb347802e29228b573c7f2c4a42efa06d6270b
buccella <buccella>
2010-12-21 23:01:49 +0000

    [ 3130727 ] CMPIPropertyMIFT.setProperty() missing objectpath keys

--- cimXmlRequest.c
+++ cimXmlRequest.c
@@ -2234,10 +2234,10 @@ static RespSegments setProperty(CimXmlRequestContext * ctx, RequestHdr * hdr)
    _SFCB_ENTER(TRACE_CIMXMLPROC, "setProperty");
    CMPIObjectPath *path;
    CMPIInstance *inst;
-   CMPIType t;
+   CMPIType t, type;
    CMPIStatus rc;
-   CMPIValue val;
-   int irc;
+   CMPIValue val, *valp;
+   int irc, i, m;
    BinRequestContext binCtx;
    BinResponseHdr *resp;
    SetPropertyReq sreq = BINREQ(OPS_SetProperty, 3);
@@ -2247,6 +2247,13 @@ static RespSegments setProperty(CimXmlRequestContext * ctx, RequestHdr * hdr)
    hdr->className=req->op.className.data;
 
    path = TrackedCMPIObjectPath(req->op.nameSpace.data, req->instanceName.className, &rc);
+   for (i = 0, m = req->instanceName.bindings.next; i < m; i++) {
+      valp = getKeyValueTypePtr(req->instanceName.bindings.keyBindings[i].type,
+                                req->instanceName.bindings.keyBindings[i].value,
+                                &req->instanceName.bindings.keyBindings[i].ref,
+                                &val, &type, req->op.nameSpace.data);
+      CMAddKey(path, req->instanceName.bindings.keyBindings[i].name, valp, type);
+   }
 
    inst = internal_new_CMPIInstance(MEM_TRACKED, NULL, NULL, 1);
 
